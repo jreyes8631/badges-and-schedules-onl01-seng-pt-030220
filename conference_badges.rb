7f6987345960764be0dc8857ae3712c8ff139edf
Jose E Reyes <jreyes8631@gmail.com>
2020-03-10 02:12:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,11 +1,5 @@
 # Write your code here.
 
 batch_badge_creator = ["Charles","Alan","Grace","Linus","Matz"]
-  new_room_1=[]
-  new_room_2=[]
-  new_room_3=[]
-  new_room_4=[]
-  new_room_5=[]
-  new_room_6=[]
-  new_room_7=[]
+
  
